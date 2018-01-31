8e5b0d29a80da6b2c8b6ad7d4948e38e5f2334ec
arrizaqu <arrizaqu@yahoo.com>
2018-01-31 17:17:14 +0700

    update note

--- tutorial-master/src/main/java/my/vaadin/app/MyUI.java
+++ tutorial-master/src/main/java/my/vaadin/app/MyUI.java
@@ -1,5 +1,6 @@
 package my.vaadin.app;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.annotation.WebServlet;
@@ -36,21 +37,70 @@ public class MyUI extends UI {
     private TextField filterText = new TextField();
     private CustomerForm form = new CustomerForm(this);
 
+    public List<User> getAllUser(){
+    	List<User> listUser = new ArrayList();
+    	User user1 = new User();
+    	User user2 = new User();
+    	User user3 = new User();
+    	User user4 = new User();
+    	User user5 = new User();
+    	User user6 = new User();
+    	
+    	user1.setId("1");
+    	user1.setAddress("seputih banyak");
+    	user1.setJobDesc("training");
+    	user1.setName("arrizaqu@yahoo.com");
+    	
+    	user2.setId("2");
+    	user2.setAddress("metro");
+    	user2.setJobDesc("finance");
+    	user2.setName("danang");
+    	
+    	user3.setId("3");
+    	user3.setAddress("datang");
+    	user3.setJobDesc("bendahara");
+    	user3.setName("agung");
+    	
+    	user4.setId("4");
+    	user4.setAddress("jakarta tangerang");
+    	user4.setJobDesc("developer");
+    	user4.setName("arrizaqu@yahoo.com");
+    	
+    	user5.setId("5");
+    	user5.setAddress("jakarta barat");
+    	user5.setJobDesc("long distance");
+    	user5.setName("bundo");
+    	
+    	user6.setId("6");
+    	user6.setAddress("sulawesi");
+    	user6.setJobDesc("Bambang sinr");
+    	user6.setName("lulu tobing");
+    	
+    	listUser.add(user1);
+    	listUser.add(user2);
+    	listUser.add(user3);
+    	listUser.add(user4);
+    	listUser.add(user5);
+    	listUser.add(user6);
+    	
+    	return listUser;
+    }
+    
+    Grid<User> dataGrid = new Grid<User>();
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         
-    	//vaadin hello world
-    	TextField textField = new TextField("name");
+    	//datatable with grid
+    	
+    	
+    	List<User> listUser= this.getAllUser();
     	
     	//layouting
     	VerticalLayout layout = new VerticalLayout();
+    	layout.addComponent(dataGrid);
+    	dataGrid.setItems(listUser);
     	
-    	Button button = new Button("click me!");
-    	button.addClickListener(e -> Notification.show("hello "+ textField.getValue()));
-    	setContent(button);
-    	
-    	layout.addComponent(textField);
-    	layout.addComponent(button);
+    	//updateList();
     	layout.setMargin(true);
     	layout.setSpacing(true);
     	setContent(layout);
